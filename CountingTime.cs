ad50711b4e8d1c2b281e904bd736470f085666e0
Ulanbek Bolotbekov <drreal@mail.ru>
2021-12-29 21:29:36 +0300

    Добавьте файлы проекта.

---
+++ CountingTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountingTheAmountOfTime
+{
+    public static class CountingTime
+    {
+        public static void CountTime()
+        {
+            
+            TimeSpan time = new TimeSpan();
+            Console.WriteLine("Для выхода нажмите \"ctrl + Q\"");
+            Console.WriteLine("Введите время в формате: чч:мм");
+            while ((Console.ReadKey().Modifiers != ConsoleModifiers.Control && Console.ReadKey(true).Key != ConsoleKey.Q))
+            {
+
+                TimeSpan inputTime = TimeSpan.Parse(Console.ReadLine());
+                time += inputTime;
+
+            }
+            Console.WriteLine($"{time.Hours:d2}:{time.Minutes:d2}");
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountingTheAmountOfTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountingTime.CountTime();
+        }
+    }
+}
